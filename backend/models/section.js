5d16e6b018cd5c8fcb3137ef85ce965c05afb900
Duncan MacKenzie <duncan@oxd.com>
2024-12-17 15:01:00 -0800

    CMS-348: wip: add section model

---
+++ backend/models/section.js
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  class Section extends Model {
+    // Helper method for defining associations.
+    // This method is not a part of Sequelize lifecycle.
+    // The `models/index` file will call this method automatically.
+    static associate(models) {
+      Section.hasMany(models.ManagementArea, {
+        foreignKey: "sectionId",
+        as: "managementAreas",
+      });
+    }
+  }
+
+  Section.init(
+    {
+      strapiId: DataTypes.INTEGER, // `id` in strapi
+      number: DataTypes.INTEGER, // `sectionNumber` in strapi
+      name: DataTypes.STRING, // `sectionName` in strapi
+      strapiUpdated: DataTypes.DATE, // `updatedAt` in strapi
+    },
+    {
+      sequelize,
+      modelName: "Section",
+    },
+  );
+
+  return Section;
+};
